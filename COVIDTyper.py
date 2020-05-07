f6262ff1d590b61eea4094333618f4c215850364
s153002 <s153002@student.dtu.dk>
2020-05-07 09:24:56 +0200

    update

--- COVIDTyper.py
+++ COVIDTyper.py
@@ -16,6 +16,8 @@ import COVIDTyperFunctions as mtf
 parser = argparse.ArgumentParser(description='.')
 parser.add_argument('-i_path_illumina', action="store", type=str, dest='i_path_illumina', default="", help='The path to the directory containing ONLY the input illumina files. Should be used when analyzing >5 read-files at a time.')
 parser.add_argument('-i_path_nanopore', action="store", type=str, dest='i_path_nanopore', default="", help='The path to the directory containing ONLY the input nanopore files. Should be used when analyzing >5 read-files at a time.')
+parser.add_argument('-i_mfa', action="store", type=str, dest='i_mfa', default="", help='Multi fasta assembly input. Only takes one single multi fasta file as input. Please give complete path to the file, not the directory.')
+parser.add_argument('-assemblies', action="store_true", dest='assemblies', default=False, help='Use this option if you are using assemblies and not raw reads.')
 parser.add_argument("-pe", action="store_true", dest="paired_end", default = False, help="If paipred ends are used give input as True (-pe True). If Paired-ends are used, it is important that the files are written in the correct order, such as: sample1_1.fasta sample1_2.fasta sample2_1.fasta sample2_1.fasta")
 parser.add_argument("-bc", action="store", type=float, default = 0.7, dest="bc", help="Base calling parameter for nanopore KMA mapping. Default is 0.7")
 parser.add_argument("-db", action="store", type=str, default = "", dest="ref_kma_database", help="Comeplete path for the ref_kma_database for KMA mapping")
@@ -26,11 +28,13 @@ parser.add_argument("-exepath", action="store", dest="exepath", default = "", he
 parser.add_argument("-o", action="store", dest="output_name", help="Name that you would like the output directory to be called.")
 args = parser.parse_args()
 
-def researchPipeline(i_path_illumina, i_path_nanopore, paired_end, bc,
-                     ref_kma_database, multi_threading, reference, output_name, exepath):
+def researchPipeline(i_path_illumina, i_path_nanopore, i_mfa, paired_end, bc,
+                     ref_kma_database, multi_threading, reference, output_name, exepath, assemblies):
+    if assemblies == True:
+        assembly_flag = True
+    else:
+        assembly_flag = False
 
-    if i_path_illumina == "" and i_path_nanopore == "":
-        sys.exit("You did not give any input. Run the program again with an input")
     #if used on server and output path is provided:
     if output_name[0] == "/":
         target_dir = output_name + "cgeout/"
@@ -55,8 +59,9 @@ def researchPipeline(i_path_illumina, i_path_nanopore, paired_end, bc,
 
     kma_path = exepath + "kma/kma"
 
-    # Print messages
 
+    # Print messages
+    #Very old logfile, need update
     startTime = time.time()
     print("# Running COVIDTyper 1.0.0 with following input conditions:", file=logfile)
     mtf.logfileConditionsResearch(logfile, bc, ref_kma_database, multi_threading, reference, output_name, paired_end)
@@ -71,6 +76,45 @@ def researchPipeline(i_path_illumina, i_path_nanopore, paired_end, bc,
     if reference != "":
         print("# -ref: " + reference, file=logfile)
     print ("loading input")
+    print ("MFA: {}".format(i_mfa))
+
+    if i_mfa != "":
+        if i_path_illumina != "": #Autodetection on CGE webserver
+            i_mfa = i_path_illumina + os.listdir(i_path_illumina)[0]
+            print (i_mfa, file = logfile)
+
+
+        assembly_flag = True
+        print ("Multifasta file was given as input.", file=logfile)
+        cmd = "mkdir {}singlefastas".format(target_dir)
+        os.system(cmd)
+        cmd = "chmod 775 " + target_dir + "singlefastas/"
+        os.system(cmd)
+        try:
+            infile = open(i_mfa, 'r')
+            sequence = ""
+            for line in infile:
+                line = line.rstrip()
+                if line[0] == ">" and sequence != "":
+                    print (sequence, file=outfile)
+                    outfile.close()
+                    sequence = ""
+                    filename = line.split()[0][1:]
+                    outfile = open("{}singlefastas/{}".format(target_dir, filename), 'w')
+                    print (line, file = outfile)
+                elif line[0] == ">" and sequence == "":
+                    filename = line.split()[0][1:]
+                    outfile = open("{}singlefastas/{}".format(target_dir, filename), 'w')
+                    print(line, file=outfile)
+                else:
+                    sequence += line
+            print(sequence, file=outfile)
+            outfile.close()
+        except IOError as err:
+            print ("IOERROR", file=logfile)
+            print("Cant open file:", str(err));
+            sys.exit(1)
+        i_path_illumina = "{}singlefastas/".format(target_dir)
 
     illumina_files = mtf.load_illumina(i_path_illumina)
     nanopore_files = mtf.load_nanopore(i_path_nanopore)
@@ -78,7 +122,7 @@ def researchPipeline(i_path_illumina, i_path_nanopore, paired_end, bc,
     nanopore_files = mtf.generate_complete_path_nanopore_files(nanopore_files, i_path_nanopore)
     total_filenames = mtf.combine_input_files(illumina_files, nanopore_files)
 
-    if 3 > len(i_path_nanopore) + len(illumina_files):
+    if 3 > len(nanopore_files) + len(illumina_files):
         sys.exit("You did not supply 2 or more input files. Please run the program again with correct input")
 
 
@@ -98,9 +142,12 @@ def researchPipeline(i_path_illumina, i_path_nanopore, paired_end, bc,
 
     ccphylo_path = exepath + "ccphylo/ccphylo"
 
-
-    cmd = "{} dist -i {}*.fsa -o {}{} -r \"{}\" -f 9 -nm 0 -nv {}nucleotideVarriance &>> {}distance_matrix_logfile".format(ccphylo_path, target_dir, target_dir, "distmatrix.phy", templatename, target_dir, target_dir)
-    os.system(cmd)
+    if assembly_flag == True:
+        cmd = "{} dist -i {}*.fsa -o {}{} -r \"{}\" -f 9 -mc 1 -nm 0 -nv {}nucleotideVarriance.gz &>> {}distance_matrix_logfile".format(ccphylo_path, target_dir, target_dir, "distmatrix.phy", templatename, target_dir, target_dir)
+        os.system(cmd)
+    else:
+        cmd = "{} dist -i {}*.fsa -o {}{} -r \"{}\" -f 1 -mc 1 -nm 0 -nv {}nucleotideVarriance.gz &>> {}distance_matrix_logfile".format(ccphylo_path, target_dir, target_dir, "distmatrix.phy", templatename, target_dir, target_dir)
+        os.system(cmd)
 
 
     infile = open("{}distance_matrix_logfile".format(target_dir),'r')
@@ -132,7 +179,11 @@ def researchPipeline(i_path_illumina, i_path_nanopore, paired_end, bc,
     mtf.varriansfileRenamer(total_filenames)
 
 def main():
-    researchPipeline(args.i_path_illumina, args.i_path_nanopore, args.paired_end, args.bc, args.ref_kma_database, args.multi_threading, args.reference, args.output_name, args.exepath)
+    inputCheck = args.i_path_illumina + args.i_path_nanopore + args.i_mfa
+    if inputCheck == "":
+        sys.exit("No input was given.")
+
+    researchPipeline(args.i_path_illumina, args.i_path_nanopore, args.i_mfa, args.paired_end, args.bc, args.ref_kma_database, args.multi_threading, args.reference, args.output_name, args.exepath, args.assemblies)
 if __name__== "__main__":
     main()
 
